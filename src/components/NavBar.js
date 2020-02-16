c32dda4eeedf05a5b792a539ce02f8564c446c5c
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-02-15 21:54:27 -0600

    Maquetacion del header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './styles/NavBar.css'
 
 class NavBar extends React.Component{
     render(){
         return(
-            <h1>Hola Platzi</h1>
+            <div className="header">
+                <div className="header--logo">
+                    <a className="header--logo__logo" href="/"></a>
+                    <a className="header--logo__letras" href="/"></a>
+                </div>
+                <div className="header--enlaces">
+                    <ul className="header--enlaces__lista">
+                        <li><a href=""> Clases </a></li>
+                        <li><a href=""> Blog </a></li>
+                        <li><a href=""> Foro </a></li>
+                        <li><a href=""> Agenda </a></li>
+                        <li><a href="">TV </a></li>
+                        <li><a href="">Contactanos </a></li>
+                    </ul>
+                </div>
+                <div className="header--usuario">
+                    <img className="header--usuario__image" src="../images/logo.webp" alt=""/>
+                    <span className="header--usuario__score" >10.619 pts </span>
+                    <img className="header--usuario__notifications" src="" alt=""/>
+                    <form>
+                        <input className="header--usuario__search" type="text" placeholder="Buscar en Platzi" />
+                        <button className="header--usuario__button">Buscar </button>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
