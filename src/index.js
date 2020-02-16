bfde83e89d4425c4dc4bb881b9c26ca06f37883d
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-02-15 20:10:10 -0600

    Empezando con el navbar y agregando estilos globales

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import './global.css'
+
+const container = document.getElementById('app')
+
+ReactDOM.render(<App />, container);
